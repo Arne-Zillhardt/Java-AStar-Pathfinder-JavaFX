dcbc9f37b845de61e1ca8cc5e5b014c97b68f398
Arne-Zillhardt <arne_zillhardt@outlook.com>
2022-07-27 11:49:33 +0200

    Set up

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import GUI.GUI;
+import static javafx.application.Application.launch;
+
+public class Main {
+    public static void main(String[] args) {
+        launch(GUI.class, args);
+    }
+}
